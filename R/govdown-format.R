761bc3c6a2f59753223081592a09144b7178af68
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2019-10-11 10:33:54 +0100

    Make examples run with local file

--- R/govdown-format.R
+++ R/govdown-format.R
@@ -95,15 +95,14 @@
 #'     google_analytics: "UA-45097885-11"
 #' ```
 #'
+#' @return R Markdown output format to pass to [rmarkdown::render()]
+#'
 #' @examples
-#' \dontrun{
-#' library(rmarkdown)
-#' library(govdown)
-#'
-#' render("input.Rmd", govdown_document())
-#'
-#' render("input.Rmd", govdown_document(phase = "alpha"))
-#' }
+#' input_rmd <- system.file("extdata/input.Rmd", package = "govdown")
+#' x <- rmarkdown::render(input_rmd, govdown_document())
+#' y <- rmarkdown::render(input_rmd, govdown_document(phase = "alpha"))
+#' utils::browseURL(x)
+#' utils::browseURL(y)
 #' @export
 govdown_document <- function(keep_md = FALSE,
                              font = c("sans-serif", "new-transport"),
