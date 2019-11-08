fb23859dc9077dbebffbac03c972264dc1f9d363
Duncan Garmonsway <nacnudus@gmail.com>
2019-11-08 12:35:30 +0000

    Try removing stringify for details

--- inst/rmarkdown/resources/govuk.lua
+++ inst/rmarkdown/resources/govuk.lua
@@ -42,7 +42,7 @@ return {
           '<details class="govuk-details" data-module="govuk-details">' ..
           '<summary class="govuk-details__summary">' ..
           '<span class="govuk-details__summary-text">'..
-          pandoc.utils.stringify(el.attributes["summary"]) ..
+          el.attributes["summary"] ..
           '</span>'..
           '</summary>' ..
           '<div class="govuk-details__text">'
